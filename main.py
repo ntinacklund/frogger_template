fafeafccfebd9e9b07c31c299987519918fd7020
Niclas L <niclas.lund@ntig.se>
2023-10-05 16:51:53 +0200

    Files added for template.

---
+++ main.py
@@ -0,0 +1,14 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+def initialize_screen():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
